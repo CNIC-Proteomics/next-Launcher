34e2b19e2c09dcaca27a1ef65c5b93993211f236
jmrodriguezc <jmrodriguezc1977@gmail.com>
2024-04-04 07:03:32 +0200

    developing

--- app/src/components/Launcher.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Launcher = () => {
-  return <div>This is the Launcher component</div>;
-};
-
-export default Launcher;
-
--- app/src/components/MainPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const MainPage = () => {
-  return <div>This is the Main Page</div>;
-};
-
-export default MainPage;
-
